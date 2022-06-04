fe9664991f21a61dc8ff9eafeb483863e71efc96
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-04 18:46:39 +0200

    Working on input's parsing logic

--- philo/philo.c
+++ philo/philo.c
@@ -6,13 +6,37 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 15:41:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/04 15:44:01 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/06/04 18:44:26 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	set_natural(char *str, int *natural)
+{
+	*natural = ft_atoi(str);
+	if (*natural < 0)
+		return (ERROR_NAN_CODE);
+	return (SUCCESS);
+}
+
+int	proccess_args(int argc, char **argv, t_philo *philo)
+{
+	if (argc < 4 || argc > 5)
+		return (ERROR_ARGS_CODE);
+	
+	
+	return (SUCCESS);
+}
+
 int	main(int argc, char **argv)
 {
+	t_philo	philo;
+
+	if (!proccess_args(argc, argv, &philo))
+	{
+		ft_putstr(ERROR_ARGS);
+		return (0);
+	}
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 15:32:39 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/04 15:40:35 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/06/04 18:44:19 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,29 @@
 // pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
 #include <pthread.h>
 
+// ******** Custom structs ********
+
+typedef struct s_philo
+{
+	int	n_philo;
+	int	t_die;
+	int	t_eat;
+	int	t_sleep;
+	int	n_times;
+} t_philo;
+
+
 // ******** Custom functions ********
+
+
+// ******** Custom messages ********
+
+# define SUCCESS 0
+# define ERROR_ARGS_CODE 1
+# define ERROR_NAN_CODE 2
+
+# define ERROR_ARGS "Use: ./philo <philos> <t_die> <t_eat> <t_sleep> [times]\n"
+# define ERROR_NANATURAL "Error: All arguments must be positive integers\n"
+
 
 #endif
