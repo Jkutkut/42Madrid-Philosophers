e20b78ac611707bf9faee7c5a1c17181044a794c
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-04 15:40:45 +0200

    Header created with needed includes

---
+++ philo/philo.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/04 15:32:39 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/06/04 15:40:35 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+// ******** Allowed functions ********
+
+// memset
+#include <string.h>
+
+// printf
+#include <stdio.h>
+
+// malloc, free
+#include <stdlib.h>
+
+// write, usleep
+#include <unistd.h>
+
+// gettimeofday
+#include <sys/time.h>
+
+// pthread_create, pthread_detach, pthread_join, pthread_mutex_init
+// pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
+#include <pthread.h>
+
+// ******** Custom functions ********
+
+#endif
