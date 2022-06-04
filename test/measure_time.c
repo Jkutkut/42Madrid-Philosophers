86595eaf7005c859d2ea99de84a35315fe92b955
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-04 17:47:13 +0200

    mimir function updated to work better

--- test/measure_time.c
+++ test/measure_time.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 15:59:33 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/04 16:58:29 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/06/04 17:47:08 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,14 @@ void	mimir(long ms)
 {
 	long	now;
 
-	if (ms > 1000)
-	{
-		usleep((long) (0.9 * ms * 1000));
-		ms = ms / 10;
-	}
 	now = get_ms();
+	if (ms > 1000)
+		usleep((ms >> 1) * 1000);
 	while (get_ms() - now < ms)
 		;
 }
+
+#define TIME 12000
 
 int	main(int argc, char **argv)
 {
@@ -47,13 +46,13 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 	{
 		start = get_ms();
-		mimir(1200);
+		mimir(TIME);
 		end = get_ms();
 	}
 	else
 	{
 		start = get_ms();
-		usleep(1200 * 1000);
+		usleep(TIME * 1000);
 		end = get_ms();
 	}
 	printf("%ld\n", end - start);
