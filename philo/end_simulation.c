11537367e32a1527d6c9ed4fe88ff8ae8d7ce37b
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-05 20:35:31 +0200

    mutex destroyed when ending simulation

--- philo/end_simulation.c
+++ philo/end_simulation.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 14:10:20 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/05 15:24:42 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/06/05 20:31:12 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,9 @@ void	end_simulation(t_simulation *info)
 	i = -1;
 	while (++i < info->n_philo)
 		pthread_join(info->philos[i].thread_id, NULL);
+	i = -1;
+	while (++i < info->n_philo)
+		pthread_mutex_destroy(&info->philos[i].fork_mtx);
+	pthread_mutex_destroy(&info->print_mtx);
 	free(info->philos);
 }
