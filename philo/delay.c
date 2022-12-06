7c55c8a78c6f9e2f89d2714120ce88706c9d747b
jkutkut <jre-gonz@student.42madrid.com>
2022-12-06 12:26:26 +0100

    delay function working better in MacOS

--- philo/delay.c
+++ philo/delay.c
@@ -6,29 +6,25 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 17:51:07 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/06/19 19:08:52 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/12/06 12:26:06 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-/*
-void	delay(long ms)
-{
-	// long	now;
-
-	// now = get_ms();
-	// if (ms > 1000)
-	// 	usleep((ms >> 1) * 1000);
-	// while (get_ms() - now < ms)
-	// 	;
-}*/
 /**
  * @brief Custom function that waits the given amount of milliseconds.
- * 
+ * The use of usleep is now good enough.
+ *
  * @param ms Amount of milliseconds to wait.
  */
 void	delay(long ms)
 {
-	usleep(ms * 1000);
+	long	t;
+
+	t = now();
+	if (ms > 1000)
+		usleep((ms >> 1) * 1000);
+	while (now() - t < ms)
+		;
 }
